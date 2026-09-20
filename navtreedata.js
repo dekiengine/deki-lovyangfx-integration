d50b9991f1e8e6d394fba9c1214d89784d8419ee
Kirbyrawr <devkirbyrawr@gmail.com>
2026-09-20 21:04:52 +0200

    Docs for deki-lovyangfx-integration from deki-editor d152da0

--- navtreedata.js
+++ navtreedata.js
@@ -32,9 +32,10 @@ var NAVTREE =
       [ "LGFXTouchPanel", "index.html#c-lgfxtouchpanel", null ]
     ] ],
     [ "Changelog", "index.html#autotoc_md3", [
-      [ "0.15.0", "index.html#autotoc_md4", null ],
+      [ "Unreleased", "index.html#autotoc_md4", null ],
       [ "Changed", "index.html#autotoc_md5", null ],
-      [ "Fixed", "index.html#autotoc_md6", null ]
+      [ "0.16.0", "index.html#autotoc_md6", null ],
+      [ "Changed", "index.html#autotoc_md7", null ]
     ] ]
   ] ]
 ];
--- navtreeindex0.js
+++ navtreeindex0.js
@@ -8,6 +8,7 @@ var NAVTREEINDEX0 =
 "index.html#autotoc_md4":[3,0],
 "index.html#autotoc_md5":[3,1],
 "index.html#autotoc_md6":[3,2],
+"index.html#autotoc_md7":[3,3],
 "index.html#c-lgfxdisplaypanel":[2,0],
 "index.html#c-lgfxtouchpanel":[2,1],
 "pages.html":[]
